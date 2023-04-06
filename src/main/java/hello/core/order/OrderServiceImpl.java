7a89c4b71930ebd5af1510569b21d0d966f2c5a5
Giyong Gim <gygim.dev@gmail.com>
2023-04-06 12:55:12 +0900

    생성자 주입을 선택하라

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -12,7 +12,7 @@ public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    @Autowired
+    // 생성자가 1개 있으면 Autowired 를 생략할 수 있다.
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
